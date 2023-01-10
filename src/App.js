683708c6175ef4d7badaf3b9be4c8e6711786f87
Jhonatan R. Saumeth <jhriverasa.edu.co>
2023-01-10 01:19:15 -0500

    added routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import Directory from "./components/directory/directory.component";
+import { Routes, Route } from "react-router-dom";
 
-const App = () => {
-  return <Directory />;
+import Navigation from "./routes/navigation/navigation.component";
+import Home from "./routes/home/home.component";
+
+const Shop = () => {
+  return <h1>I am the shop page</h1>;
 };
 
+const App = () => {
+  return (
+    <Routes>
+      <Route Route path="/" element={<Navigation />}>
+        <Route index={true} element={<Home />} />
+        <Route path="shop" element={<Shop />} />
+      </Route>
+    </Routes>
+  );
+};
 
 export default App;
